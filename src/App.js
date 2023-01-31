2751c2fa9b65a7d0fa9bd8c39d288e3b30b7e330
safaezouadi2023 <safae.zouadi2023@gmail.com>
2023-01-31 18:08:37 +0100

    kkk

--- src/App.js
+++ src/App.js
@@ -6,7 +6,13 @@ function App() {
     <div className="App">
      
 
-     <h1>   My Contact App</h1>
+     <h1>   My Contact App update</h1>
+
+
+     <h2>     Hello </h2>
+
+
+     <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus molestias minima, consequatur quidem eum voluptatem, quas esse possimus quam odio hic quo sapiente, totam obcaecati itaque placeat deserunt beatae sed. </p>
     </div>
   );
 }
